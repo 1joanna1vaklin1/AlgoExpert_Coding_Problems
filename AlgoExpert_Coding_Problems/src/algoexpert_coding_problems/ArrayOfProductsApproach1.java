84e2de23a33fc22b103169872d5649f706f1e3c1
1joanna1vaklin1 <46585956+1joanna1vaklin1@users.noreply.github.com>
2021-09-25 22:53:09 -0500

    Create ArrayOfProductsApproach1.java

---
+++ AlgoExpert_Coding_Problems/src/algoexpert_coding_problems/ArrayOfProductsApproach1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class ArrayOfProductsApproach1 {
+  public int[] arrayOfProducts(int[] array) {
+    
+		int [] left = new int[array.length];
+		int [] right = new int[array.length];
+		
+		Arrays.fill(left, 1); 
+		Arrays.fill(left, 1);
+		
+		for(int i = 0; i < array.length; i++)
+		{
+			
+		}
+		
+		
+    return new int[] {};
+  }
+}
