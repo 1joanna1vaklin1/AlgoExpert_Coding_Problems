2acfca2241fd6323989f2118984c977d1bbc5de5
1joanna1vaklin1 <46585956+1joanna1vaklin1@users.noreply.github.com>
2021-09-25 23:09:45 -0500

    Update ArrayOfProductsApproach1.java

--- AlgoExpert_Coding_Problems/src/algoexpert_coding_problems/ArrayOfProductsApproach1.java
+++ AlgoExpert_Coding_Problems/src/algoexpert_coding_problems/ArrayOfProductsApproach1.java
@@ -1,3 +1,9 @@
+//Array of Products Problem from AlgoExpert.io
+//Return an int array that contains the product of 
+//every number besides the indexed one.
+//Self written code with help from AlgoExpert solutions and videos. 
+//Joanna B. Vaklin 
+
 import java.util.*;
 
 public class ArrayOfProductsApproach1 {
@@ -8,13 +14,28 @@ public class ArrayOfProductsApproach1 {
 		
 		Arrays.fill(left, 1); 
 		Arrays.fill(left, 1);
+		int product = 1;
 		
 		for(int i = 0; i < array.length; i++)
 		{
-			
+			left[i] = product;
+			product *= array[i];
+		}
+	  
+		product = 1; 
+	  
+		for(int j = array.length-1; j >= 0; j--)
+		{
+			right[j] = product;
+			product *= array[j];
+		}
+		
+		for(int i = 0; i < array.length; i++)
+		{
+			products[i] = right[i] * left[i];
 		}
 		
 		
-    return new int[] {};
+    return products;
   }
 }
