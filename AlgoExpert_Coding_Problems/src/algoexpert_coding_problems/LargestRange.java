3209261ce5774427567e6bdf3917cfc141bbad49
1joanna1vaklin1 <46585956+1joanna1vaklin1@users.noreply.github.com>
2021-10-28 19:52:06 -0500

    Update LargestRange.java
    Most time efficient solution. O(n) time and O(n) space. compared to if you would sort the array and retrieve solution from that. then it would be O(nlogn)

--- AlgoExpert_Coding_Problems/src/algoexpert_coding_problems/LargestRange.java
+++ AlgoExpert_Coding_Problems/src/algoexpert_coding_problems/LargestRange.java
@@ -1,53 +1,70 @@
-//IN PROGRESS
-
+//LargestRange problem from AlgoExpert.io
+//Self written code with help on the algorithm 
+//from AlgoExpert.io solutions, hints, and video
+//Joanna B. Vaklin
 
 import java.util.*;
 
 public class LargestRange {
+	
+	//one example in the main method
+	//it should return [0, 7] since that range exists in the int array
+	//and is the greatest of all ranges that exist in the array
+	public static void main(String [] args)
+	{
+		int [] array = new int[] {1, 11, 3, 0, 15, 5, 2, 4, 10, 7, 12, 6};
+		
+		int [] largestRange = largestRange(array); 
+	}
+	
   public static int[] largestRange(int[] array) {
-    
-		Arrays.sort(array);
+
+	Map<Integer, Boolean> nums = new HashMap<Integer, Boolean>();
+ 	int currentRangeCount = 0; 
+	int longestRange = Integer.MIN_VALUE;
+	int [] range = new int[2]; 
 		
-		int prev = array[0];
-		int current = 0;
-		boolean continuingRange = false;
-		int currentX = 0;
-		int currentY = 0;
-		int count = 0; 
-		int currentMax = Integer.MIN_VALUE;
-		int firstIndex = 0;
-		int lastIndex = 0;
-		
-		for(int i = 1; i < array.length; i++)
+		for(int num : array)
 		{
-			current = array[i]; 
-			if(current == (prev) + 1)
-			{
-				if(continuingRange)
-				{
-					currentY = current; 
-					count++;
-				}
-				else
-				{
-					currentX = prev;
-					currentY = current;
-					continuingRange = true; 
-					count = 1;
-				}
-			}
-			else
-			{
-				if(count > currentMax)
-				{
-					currentMax = count; 
-					firstIndex = currentX;
-					lastIndex = currentY;
-				}
-				count = 0;
-			}
+			nums.put(num, false);
 		}
 		
-    return new int[] {firstIndex, lastIndex};
+		for(int num : array)
+		{
+			//meaning it was an already visited num in another range
+			if(nums.get(num))
+				continue;
+			
+			else
+			{
+				nums.put(num, true);
+				int minusNum = num - 1;
+				int plusNum = num + 1;
+				currentRangeCount = 1; 
+				
+				//checking all the numbers to the left of currentNum
+				while(nums.containsKey(minusNum))
+				{
+					nums.put(minusNum, true);
+					currentRangeCount++; 
+					minusNum--; 
+				}
+ 				//checking all the numbers to the right of currentNum
+				while(nums.containsKey(plusNum))
+				{
+					nums.put(plusNum, true);
+					currentRangeCount++;
+					plusNum++;
+				}
+				//updating LongestRange
+				if(currentRangeCount > longestRange)
+				{
+					longestRange = currentRangeCount;
+					range = new int[] {minusNum + 1, plusNum - 1};
+				}
+			}
+			
+		}
+	return range;
   }
 }
